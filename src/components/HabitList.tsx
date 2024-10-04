20a481a3655b228cfc277445c8eea66db61ec20c
Yeasin <ar9485727@gmail.com>
2024-10-04 20:07:20 +0600

    design change

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -34,7 +34,7 @@ const HabitList = () => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: "2", mt: 4 }}>
       {habits.map((habit) => (
-        <Paper key={habit.id} elevation={3} sx={{ p: 2 }}>
+        <Paper key={habit.id} elevation={3} sx={{ p: 2,my:2 }}>
           <Grid container alignItems="center">
             <Grid xs={12} sm={6}>
               <Typography variant="h6">{habit.name}</Typography>
